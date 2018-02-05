ba0f793b6ca74121e086801addf988ffd4a11d8d
Porter <lporter@netsuite.com>
2018-02-05 11:05:16 -0600

    Converted .jade to .pug files
    Pugs, pugs everywhere

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('.app_server/routes/index');
-var users = require('.app_server/routes/users');
+var index = require('./app_server/routes/index');
+var users = require('./app_server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server' , 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
