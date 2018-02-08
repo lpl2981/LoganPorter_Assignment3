bde6a1d502c274ce7f1781dd7b2e21350ce36feb
Porter <lporter@netsuite.com>
2018-02-08 07:48:34 -0600

    Fixed layout and index
    Fixed layout and index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Assignment 3 - Static Webpage' });
 });
 
 module.exports = router;
